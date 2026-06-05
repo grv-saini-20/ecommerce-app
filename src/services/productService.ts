c2071de1496deca60677ad52965f470b3cd4c799
gourav <gourav@auvo.io>
2026-06-05 10:33:08 +0530

    feat: built services for fetching all products and product by id

---
+++ src/services/productService.ts
@@ -0,0 +1,21 @@
+import type { IProduct } from "../types"
+
+const BASE_URL = 'https://api.escuelajs.co/api/v1/'
+
+export const fetchAllProducts = async():Promise<IProduct[]> => {
+  const response = await fetch(`${BASE_URL}/products`)
+  if(!response.ok) {
+    throw new Error('Failed to fetch products')
+  }
+  const data = await response.json()
+  return data
+}
+
+export const fetchProductById = async(id: string):Promise<IProduct> => {
+    const response = await fetch(`${BASE_URL}/products/${id}`)
+    if(!response.ok) {
+        throw new Error('Failed to fetch product')
+    }
+    const data = await response.json()
+    return data
+}
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+    id: string,
+    title: string,
+    slug: string,
+    price: number,
+    description: string,
+    category: {
+      id: number,
+      name: string,
+      image: string,
+      slug: string
+    },
+    images: string[]
+}
